55e8e73bf07f19eee58bf2ef81d62d9cbddf6a3b
5ervant Mark Anthony B. Dungo <5ervant@protonmail.com>
2019-07-21 23:58:14 +0800

    Fixed Metric-Imperial Converter failing tests

--- information-security-and-quality-assurance/projects/project-metricimpconverter/tests/2_functional-tests.js
+++ information-security-and-quality-assurance/projects/project-metricimpconverter/tests/2_functional-tests.js
@@ -1,10 +1,10 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
 var chaiHttp = require('chai-http');
 var chai = require('chai');
@@ -14,27 +14,26 @@ var server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
   suite('Routing Tests', function() {
-    
     suite('GET /api/convert => conversion object', function() {
-      
       test('Convert 10L (valid input)', function(done) {
-       chai.request(server)
-        .get('/api/convert')
-        .query({input: '10L'})
-        .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
-          assert.equal(res.body.returnUnit, 'gal');
-          done();
-        });
+        chai
+          .request(server)
+          .get('/api/convert')
+          .query({ input: '10L' })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, 10);
+            assert.equal(res.body.initUnit, 'l');
+            assert.approximately(res.body.returnNum, 2.64172, 0.1);
+            assert.equal(res.body.returnUnit, 'gal');
+            done();
+          });
       });
-      
+
       test('Convert 32g (invalid input unit)', function(done) {
-        chai.request(server)
+        chai
+          .request(server)
           .get('/api/convert')
           .query({ input: '32g' })
           .end(function(err, res) {
@@ -43,9 +42,10 @@ suite('Functional Tests', function() {
             done();
           });
       });
-      
+
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
-        chai.request(server)
+        chai
+          .request(server)
           .get('/api/convert')
           .query({ input: '3/7.2/4kg' })
           .end(function(err, res) {
@@ -53,10 +53,11 @@ suite('Functional Tests', function() {
             assert.equal(res.text, 'invalid number');
             done();
           });
-      });  
-      
+      });
+
       test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        chai.request(server)
+        chai
+          .request(server)
           .get('/api/convert')
           .query({ input: '3/7.2/4kilomegagram' })
           .end(function(err, res) {
@@ -65,9 +66,10 @@ suite('Functional Tests', function() {
             done();
           });
       });
-      
+
       test('Convert kg (no number)', function(done) {
-        chai.request(server)
+        chai
+          .request(server)
           .get('/api/convert')
           .query({ input: 'kg' })
           .end(function(err, res) {
@@ -79,9 +81,6 @@ suite('Functional Tests', function() {
             done();
           });
       });
-      
     });
-
   });
-
 });
